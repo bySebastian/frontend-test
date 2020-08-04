37e699f54dff0821f4f0925f3d3c82d84cfb2e95
Sebastian Vyaene <sebastian@marlon.be>
2020-08-04 22:21:15 +0200

    use arrow functions

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ function loadTests(tests, driver, url) {
 
     driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
     
-    driver.sleep(1000).then(function(){
+    driver.sleep(1000).then(() => {
         driver.findElement(webdriver.By.name("q")).sendKeys(webdriver.Key.ENTER);
     });
     
-    driver.sleep(2000).then(function(){
-        driver.getTitle().then(function(title) {
+    driver.sleep(2000).then(() => {
+        driver.getTitle().then((title) => {
             if (title === "webdriver - Google Search") {
                 console.log("Test passed.");
             } else {
