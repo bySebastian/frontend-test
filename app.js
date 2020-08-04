91fc897642cebb931b0f051e0d3a14a6080a25d7
Sebastian Vyaene <sebastian@marlon.be>
2020-08-04 22:15:15 +0200

    fix check to load url as param

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const webdriver = require("selenium-webdriver");
 const CONFIG = require("./config.json");
+const url = process.argv.slice(2);
 
 if (CONFIG.browsers && CONFIG.browsers.length) {
-    CONFIG.browsers.map((browser) => {
-        require(browser.driver);
-        const driver = new webdriver.Builder("").forBrowser(browser.name).build();
-        loadTests(driver);
-    });
+    if (url && url.length) {
+        CONFIG.browsers.map((browser) => {
+            require(browser.driver);
+            const driver = new webdriver.Builder("").forBrowser(browser.name).build();
+            loadTests(CONFIG.tests, driver, url[0]);
+        });
+    } else {
+        console.log("Test failed. URL parameter is missing.");
+    }
 }
 
-function loadTests(driver) {
+function loadTests(tests, driver, url) {
+    console.log(tests);
 
-    const url = process.argv.slice(2);
+    driver.get(url);
 
-    if (url && url.length) {
-
-        driver.get(url[0]);
-
-        driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
-        
-        driver.sleep(1000).then(function(){
-            driver.findElement(webdriver.By.name("q")).sendKeys(webdriver.Key.ENTER);
+    driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
+    
+    driver.sleep(1000).then(function(){
+        driver.findElement(webdriver.By.name("q")).sendKeys(webdriver.Key.ENTER);
+    });
+    
+    driver.sleep(2000).then(function(){
+        driver.getTitle().then(function(title) {
+            if (title === "webdriver - Google Search") {
+                console.log("Test passed.");
+            } else {
+                console.log("Test failed.");
+            }
+            driver.quit();
         });
-        
-        driver.sleep(2000).then(function(){
-            driver.getTitle().then(function(title) {
-                if (title === "webdriver - Google Search") {
-                    console.log("Test passed.");
-                } else {
-                    console.log("Test failed.");
-                }
-                driver.quit();
-            });
-        });
-    }
-
+    });
 }
