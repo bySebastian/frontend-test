9205a90fcbe7545f4c40b99fd839700ec5b5e8cb
Sebastian Vyaene <sebastian@marlon.be>
2020-08-04 16:35:43 +0200

    fix test

--- google_test.js
+++ google_test.js
@@ -17,10 +17,8 @@ function searchTest(driver) {
     driver.findElement(By.name("q")).sendKeys("webdriver");
     
     driver.sleep(1000).then(function(){
-        driver.findElement(By.name("q")).sendKeys(webdriver.Key.TAB);
+        driver.findElement(By.name("q")).sendKeys(webdriver.Key.ENTER);
     });
-    
-    driver.findElement(By.name("btnK")).click();
     
     driver.sleep(2000).then(function(){
         driver.getTitle().then(function(title) {
