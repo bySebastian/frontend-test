8299c1929007270d908c0f7ccdec1eb34fadabf2
Sebastian Vyaene <sebastian@marlon.be>
2020-08-04 21:50:34 +0200

    update: load url from a parameter

--- app.js
+++ app.js
@@ -5,27 +5,34 @@ if (CONFIG.browsers && CONFIG.browsers.length) {
     CONFIG.browsers.map((browser) => {
         require(browser.driver);
         const driver = new webdriver.Builder("").forBrowser(browser.name).build();
-        searchTest(driver);
+        loadTests(driver);
     });
 }
 
-function searchTest(driver) {
-    driver.get("https://www.google.com");
+function loadTests(driver) {
 
-    driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
-    
-    driver.sleep(1000).then(function(){
-        driver.findElement(webdriver.By.name("q")).sendKeys(webdriver.Key.ENTER);
-    });
-    
-    driver.sleep(2000).then(function(){
-        driver.getTitle().then(function(title) {
-            if (title === "webdriver - Google Search") {
-                console.log("Test passed.");
-            } else {
-                console.log("Test failed.");
-            }
-            driver.quit();
+    const url = process.argv.slice(2);
+
+    if (url && url.length) {
+
+        driver.get(url[0]);
+
+        driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
+        
+        driver.sleep(1000).then(function(){
+            driver.findElement(webdriver.By.name("q")).sendKeys(webdriver.Key.ENTER);
         });
-    });
+        
+        driver.sleep(2000).then(function(){
+            driver.getTitle().then(function(title) {
+                if (title === "webdriver - Google Search") {
+                    console.log("Test passed.");
+                } else {
+                    console.log("Test failed.");
+                }
+                driver.quit();
+            });
+        });
+    }
+
 }
