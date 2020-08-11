b6734c25af82fb4dfa44ba0f198a295b7aa0c778
Sebastian Vyaene <sebastian@marlon.be>
2020-08-12 01:09:22 +0200

    fix promise error

--- app.js
+++ app.js
@@ -5,18 +5,14 @@ const url = process.argv.slice(2)[0];
 if (CONFIG.browsers && CONFIG.browsers.length) {
     if (url && url.length) {
         CONFIG.browsers.map((browser) => {
-            try {
-                if (CONFIG.tests && CONFIG.tests.length) {
-                    CONFIG.tests.map((testItem) => {
-                        Test(browser, url, testItem).then(result => {
-                            console.log("result", result);
-                        });
+            if (CONFIG.tests && CONFIG.tests.length) {
+                CONFIG.tests.map(async (testItem) => {
+                    await Test(browser, url, testItem).then(result => {
+                        console.log("result", result);
                     });
-                } else {
-                    console.log("ERROR. No tests available.");
-                }
-            }catch(error) {
-                console.log(`Error. ${error}`);
+                });
+            } else {
+                console.log("ERROR. No tests available.");
             }
         });
     } else {
