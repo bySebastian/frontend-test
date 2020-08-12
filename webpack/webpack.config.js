df86fd9633689742a589fad6b126e8ec8c648ff7
Sebastian Vyaene <sebastian@marlon.be>
2020-08-12 10:41:34 +0200

    add webpack config for scrips (es6)

---
+++ webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "app.bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["es2015"]
+                    }
+                },
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: "source-map"
+};
