f40bc3424b113a08e630d5d437643583cb9a64d2
Sebastian Vyaene <sebastian@marlon.be>
2020-08-12 10:41:15 +0200

    update code to load tests

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const getTest = require("./lib/test");
 const CONFIG = require("./config.json");
 const url = process.argv.slice(2)[0];
+let results = [];
 
 if (CONFIG.browsers && CONFIG.browsers.length) {
     if (url && url.length) {
         CONFIG.browsers.map((browser) => {
             if (CONFIG.tests && CONFIG.tests.length) {
-                CONFIG.tests.map(async (testItem) => {
-                    const result = await getTest(browser, url, testItem).then(value => {
+                CONFIG.tests.map(async (test) => {
+                    const result = await getTest(browser, url, test).then(value => {
                         return value;
                     });
-                    console.log("result", result);
+                    results.push(result);
+                    console.log("result", results);
                 });
             } else {
                 console.log("ERROR. No tests available.");
